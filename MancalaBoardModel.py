e8ba5684ba3b90df738edb8c8d7afa810f09e599
KelvinThoo <44352099+KelvinThoo@users.noreply.github.com>
2019-06-21 02:34:32 +0800

    Add files via upload

---
+++ MancalaBoardModel.py
@@ -0,0 +1,139 @@
+#Bryan Perez
+#email: perezjbryan@gmail.com
+# This file contains Mancala Board and functions for using it
+import array
+import time
+class MancalaBoardModel:
+    
+    # constructor:
+    # initializes board
+    #
+    #
+    #                        player1                    player2
+    def __init__(self, boardsize = 14): #[ 0, 1, 2, 3, 4, 5 ,6    7, 8, 9, 10, 11, 12, 13]
+#        self.boardgame = [ 4, 4, 4, 4, 4, 4, 4,      4, 4, 4, 4,  4,  4, 4]
+        self.boardgame =array.array('i', (4 for i in range(boardsize)))
+        self.tempboardgame = [ 4, 4, 4, 4, 4, 4, 4 ,      4, 4, 4,  4,  4,  4, 4]
+    
+    #Function Boardgame(self)
+    #Returns the board game array
+    def Boardgame(self):
+        return self.boardgame
+        
+    #Function ReturnHoleValue(self, hole)
+    #returns value of specific hole
+    def ReturnHoleValue(self, hole):
+        if hole < 7:
+            return self.boardgame[hole-1]
+        if hole >6:
+            return self.boardgame[hole-1]
+        
+    
+    #Function Last_stone_position(self)
+    # return the last stone position
+    def Last_stone_position(self):
+        
+        return self.Last_stone_position
+              
+##
+    def MoveSelected(self, HoleNumber, playerName, continueMove = False):
+      
+        
+        currentHole = self.PreviousPit(HoleNumber - 1 )
+        self.tempboardgame = list(self.boardgame)
+        
+        
+      
+#        Loop using number of stone in hand
+        while True:
+            if self.boardgame[self.NextPit(currentHole)] == 0:
+                score =  self.boardgame[self.NextPit(self.NextPit(currentHole))]
+                self.boardgame[self.NextPit(self.NextPit(currentHole))] = 0
+                print(score)
+                return score
+            currentHole = self.NextPit(currentHole)
+            stones = self.boardgame[currentHole]
+            self.boardgame[currentHole] = 0
+
+            while stones != 0:
+    #            Go to next hole
+                currentHole = self.NextPit(currentHole);
+    #            Add to to current(previously next) hole
+                self.boardgame[currentHole] += 1
+                stones -= 1 
+                
+    #            Check if next hole is empty
+
+            
+                    
+    
+
+    def NextPit(self, hole):
+        if hole == (len(self.boardgame)- 1):
+            return 0
+        
+        return (hole + 1) 
+      
+    def PreviousPit(self, hole):
+        if hole == 0:
+            return (len(self.boardgame)- 1)
+        
+        return (hole-1)
+        
+    
+    def moveCheck(self, Player, HoleNumber):
+        if Player == "Player 1" and HoleNumber in range (1,7) and self.boardgame[HoleNumber] != 0:
+            return True
+        elif Player == "Player 2" and HoleNumber in range (1,7) and self.boardgame[HoleNumber] != 0:
+            return True
+        else:
+            return False
+    
+    
+    # Function HasStonesLeft(self, Player)
+    # Takes in players name checks to see if player has any stones left in his row            
+    def HasStonesLeft(self, Player):
+        StonesLeft = False;
+        total = 0
+        
+        if Player == "Player 1":
+            x=0 
+            while x < 7:
+                total = total + self.boardgame[x]
+                x=x+1
+                
+            if total > 0:
+                StonesLeft = True
+        
+        if Player == "Player 2":
+            x=7 
+            while x < 14:
+                total = total + self.boardgame[x]
+                x=x+1
+                
+            if total > 0:
+                StonesLeft = True
+                
+        return StonesLeft
+    
+   
+
+   
+    ###################################################################################################################
+    #debug functions
+    def printb(self):    
+        print ("[14][13][12][11][10][9][8]")
+        print (self.boardgame[13],self.boardgame[12], self.boardgame[11], self.boardgame[10], self.boardgame[9], self.boardgame[8], self.boardgame[7])
+        print (self.boardgame[0], self.boardgame[1], self.boardgame[2], self.boardgame[3], self.boardgame[4], self.boardgame[5],self.boardgame[6])
+        print ("[1][2][3][4][5][6][7]")
+        print ("player 1 store: ", self.boardgame[0])
+        print ("player 2 store: ", self.boardgame[1])
+        print (" ")
+        
+#    def printt(self):
+#        print (" PREVIOUS BOARD#################################")
+#        print (self.tempboardgame[13],self.tempboardgame[12], self.tempboardgame[11], self.tempboardgame[10], self.tempboardgame[9], self.tempboardgame[8], self.tempboardgame[7])
+#        print (self.tempboardgame[0], self.tempboardgame[1], self.tempboardgame[2], self.tempboardgame[3], self.tempboardgame[4], self.tempboardgame[5],self.boardgame[6])
+#        print ("player 1 store: ", self.tempboardgame[0])
+#        print ("player 2 store: ", self.tempboardgame[1])
+#        print (" PREVIOUS BOARD#################################")
---
+++ MancalaController.py
@@ -0,0 +1,188 @@
+#Bryan Perez
+#email: perezjbryan@gmail.com
+# This file contains the mancala game logic
+
+import MancalaBoardModel, PlayerModel
+
+
+
+
+class MancalaController:
+
+    def __init__(self):
+        self.board = MancalaBoardModel.MancalaBoardModel()
+        self.Current_Player = PlayerModel.PlayerModel("Player 1")
+        self.Other_Player = PlayerModel.PlayerModel("Player 2")
+        self.player_score = [0,0]
+        self.score = 0
+        
+        
+    def UpdateScore(self,score):
+        
+        if self.player_score[self.Current_Player.PlayerName == "Player 1"]:
+            x = 0
+        else:
+            x = 1
+            
+        self.player_score[x] = self.player_score[x] + score
+          
+
+    
+
+    def ContinueGame(self):
+        return self.board.HasStonesLeft(self.Other_Player.PlayerName())
+        
+
+    def ReturnHoleValue(self, hole):
+#        self.board.printb()
+        return self.board.ReturnHoleValue(hole)
+        
+    def ReturnStoresValue(self):
+        return self.board.ReturnStores()
+        
+
+    
+    def ReturnTotalScore(self):
+        player1 = 0
+        player2 = 0
+        
+        if self.Current_Player.PlayerName() == "Player 1":
+            player1 = self.player_score[0]
+        else:
+            player2 = self.player_score[1]
+            
+        scoreTuple = (player1,player2)
+        return scoreTuple
+        
+    def SwitchPlayer(self):
+        #switch player
+        if self.Current_Player.PlayerName() == "Player 1":
+            self.Current_Player.PlayerName() == "Player 2"
+        else:
+            self.Current_Player.PlayerName() == "Player 1"
+        
+        return self.Current_Player.PlayerName()
+    
+    
+    def ReturnPlayerTurn(self,):
+        return self.Current_Player.PlayerName()
+    
+    def PlayerSelectsHole(self, hole):    
+        # Updates Players Score
+        if self.Current_Player.PlayerName() == "Player 1":
+             score = self.board.MoveSelected(hole, self.Current_Player.PlayerName())
+             print(score)
+             self.player_score[0] = self.player_score[0] + score
+             print("This is store:")
+             print(self.player_score[0])
+        elif self.Current_Player.PlayerName() == "Player 2":
+             score = self.board.MoveSelected(hole, self.Current_Player.PlayerName())
+             print(score)
+             score = self.player_score[1]
+             print("This is store:")
+             print(self.player_score[1])
+        self.Current_Player.SetScore(score)
+    
+    
+    def EndofGameLogic(self):
+        # Update other players score
+        Store = self.board.ReturnStores()
+        if self.Other_Player.PlayerName() == "Player 1":
+            Score = self.Current_Player.Score()
+            Score = Store[0]
+        elif self.Other_Player.PlayerName() == "Player 2":
+            Score = self.Current_Player.Score()
+            Score = Store[1]
+        self.Other_Player.SetScore(Score)
+    
+    def DetermineWinner(self):
+        # Player with most stones wins
+        if self.Other_Player.Score() == self.Current_Player.Score():
+            return "tie"
+        elif self.Other_Player.Score() > self.Current_Player.Score():
+            return self.Other_Player.PlayerName()
+        else:
+            return self.Current_Player.PlayerName()
+    
+
+'''
+#############################################
+# Copy of original controller without UI
+#############################################
+
+# Funtion main():
+# Contains the logic of the game mancala
+def main():
+    
+    board = MancalaBoardModel.MancalaBoardModel()
+    Current_Player = PlayerModel.PlayerModel("Player 1")
+    Other_Player = PlayerModel.PlayerModel("Player 2")
+    
+
+    
+    while board.HasStonesLeft(Other_Player.PlayerName()) :
+        
+        #------------------------------------------------------------------#
+        # Display board
+        board.printb()
+        #------------------------------------------------------------------#
+        # Player selects the hole
+        print Current_Player.PlayerName(), "Your Turn"
+        print " "
+        SelectedHole = int(input("Enter choice"))
+        #------------------------------------------------------------------#
+        # Calculates Players move & sets last stone postion
+        board.MoveSelected(SelectedHole, Current_Player.PlayerName())
+        #------------------------------------------------------------------#
+        # Last stone placed in an empty spot on your side? 
+        board.LastStoneEmptySpot(Current_Player.PlayerName())            
+        #------------------------------------------------------------------#
+        # Updates Players Score
+        Store = board.ReturnStores()
+        if Current_Player.PlayerName() == "Player 1":
+            Score = Current_Player.Score()
+            Score = Score + Store[0]
+        elif Current_Player.PlayerName() == "Player 2":
+            Score = Current_Player.Score()
+            Score = Score + Store[1]
+        Current_Player.SetScore(Score)
+        # Updates Board 
+        board.printb()
+        #------------------------------------------------------------------#
+        # if last stone was placed in the players store another turn is given
+        if board.Last_stone_position < 14:
+            temp = Current_Player
+            Current_Player = Other_Player
+            Other_Player = temp
+        #------------------------------------------------------------------#
+        # Updates Board 
+        board.printb()
+        
+    
+    #------------------------------------------------------------------#
+    # Moves Other_Players remaining stones to his store
+    board.RowToStore(Other_Player.PlayerName())
+    #------------------------------------------------------------------#
+    # Update other players score
+    Store = board.ReturnStores()
+    if Other_Player.PlayerName() == "Player 1":
+        Score = Current_Player.Score()
+        Score = Score + Store[0]
+    elif Other_Player.PlayerName() == "Player 2":
+        Scores = Current_Player.Score()
+        Score = Score + Store[1]
+    Other_Player.SetScore(Score)
+    #------------------------------------------------------------------#
+    # Player with most stones wins
+    if Other_Player.Score() > Current_Player.Score():
+        print Other_Player.PlayerName(), " Wins"
+    else:
+        print Current_Player.PlayerName(), " Wins"
+    
+if __name__ == "__main__":
+    main() 
+    
+'''
+    
+
+    
